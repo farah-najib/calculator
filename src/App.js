72e8770f162252fdb7c65868866d785501fa5677
Farah Najib <farahnajib@mac.local>
2024-01-21 17:27:26 +0100

    add tab

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import MetricUnit from './components/MetricUnit'
 import UsUnit from './components/UsUnit'
 import BmiResult from './components/BmiResult'
 import { useState } from 'react'
-
+import { Tabs, Tab } from 'react-bootstrap'
 
 function App() {
     const [bmiResult, setBmiResult] = useState()
+    const [key, setKey] = useState('tab1')
     return (
         <div>
-            <MetricUnit setBmiResult={setBmiResult} />
-            <UsUnit setBmiResult={setBmiResult} />
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+            >
+                <Tab eventKey="tab1" title="Tab 1">
+                    <MetricUnit setBmiResult={setBmiResult} />
+                </Tab>
+                <Tab eventKey="tab2" title="Tab 2">
+                    {' '}
+                    <UsUnit setBmiResult={setBmiResult} />{' '}
+                </Tab>
+            </Tabs>
             <BmiResult bmiResult={bmiResult} />
         </div>
     )
